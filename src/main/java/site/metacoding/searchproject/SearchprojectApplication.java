c9ca9825c5b2771b99e6b6595dde0b0674484816
gyung <282203@naver.com>
2022-03-24 15:20:52 +0900

    4. BoardTbl2 Entity 생성 완료

--- src/main/java/site/metacoding/searchproject/SearchprojectApplication.java
+++ src/main/java/site/metacoding/searchproject/SearchprojectApplication.java
@@ -2,7 +2,9 @@ package site.metacoding.searchproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing // JPA Auditing 활성화
 @SpringBootApplication
 public class SearchprojectApplication {
 
---
+++ src/main/java/site/metacoding/searchproject/domain/BoardTbl2.java
@@ -0,0 +1,33 @@
+package site.metacoding.searchproject.domain;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@EntityListeners(AuditingEntityListener.class)
+@Entity
+public class BoardTbl2 {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String title;
+    private String content;
+
+    @CreatedDate
+    private LocalDateTime createDate;
+}
