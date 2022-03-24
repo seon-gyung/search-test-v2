8d044b0e455fd600cd960a34e266cc39a8a8a108
gyung <282203@naver.com>
2022-03-24 14:54:11 +0900

    1. 이니셜 프로젝트

---
+++ src/main/java/site/metacoding/searchproject/SearchprojectApplication.java
@@ -0,0 +1,13 @@
+package site.metacoding.searchproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/site/metacoding/searchproject/SearchprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package site.metacoding.searchproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
